56febb50a08bf34359c8d1e3a3044610c14ab180
joannawalach1 <joanna.walach@op.pl>
2024-08-07 20:13:01 +0200

    addins webclient tests

--- src/test/java/com/atipera/githubApi/controller/GithubControllerTest.java
+++ src/test/java/com/atipera/githubApi/controller/GithubControllerTest.java
@@ -1,85 +1,66 @@
 package com.atipera.githubApi.controller;
 
+import com.atipera.githubApi.domain.GithubRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.time.Duration;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GithubControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebTestClient webTestClient;
 
     @Value("${github.token}")
     private String token;
 
     @Test
-    void shouldPrintRepositoryNames() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/github/users/{userLogin}/repos", "JoannaWalach1")
-                        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].repositoryName").value("algoritms"))
-                .andReturn();
+    void shouldGetRepos() throws Exception {
+        webTestClient.mutate()
+                .responseTimeout(Duration.ofSeconds(20))
+                .build()
+                .get().uri("/api/github/users/{userLogin}/repos", "JoannaWalach1")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(GithubRepository.class);
     }
 
     @Test
-    void shouldPrintBranchesNames() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/github/repos/{userLogin}/{nameOfRepo}/branches", "JoannaWalach1", "School-App")
-                        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-                        .header("Authorization", "Token: " + token))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].branchName").value("main"))
-                .andReturn();
+    void shouldThrowExceptionIfLoginIsNull() {
+        webTestClient.get().uri("/api/github/users/{userLogin}/repos", "")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound();
     }
 
     @Test
-    void shouldThrowExceptionIfLoginIsNull() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/github/users/{userLogin}/repos", "")
-                        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isNotFound())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.status").value(404))
-                .andExpect(jsonPath("$.message").value("No handler found for GET /api/github/users//repos"))
-                .andReturn();
+    void shouldThrowExceptionIfLoginDoesNotExist() {
+        webTestClient.get().uri("/api/github/users/{userLogin}/repos", "nonexistentUser")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound();
     }
 
     @Test
-    void shouldThrowExceptionIfLoginDoesNotExist() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/github/users/{userLogin}/repos", "nonexistentUser")
-                        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isNotFound())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.status").value(404))
-                .andExpect(jsonPath("$.message").value("User 'nonexistentUser' does not exist"))
-                .andReturn();
-    }
-
-    @Test
-    void shouldThrowExceptionIfRepoNameIsNull() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/github/repos/{userLogin}/{nameOfRepo}/branches", "JoannaWalach1", "")
-                        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.status").value(404))
-                .andExpect(jsonPath("$.message").value("No handler found for GET /api/github/repos/JoannaWalach1//branches"))
-                .andReturn();
+    void shouldThrowExceptionIfRepoNameIsNull() {
+        webTestClient.get().uri("/api/github/repos/{userLogin}/{nameOfRepo}/branches", "JoannaWalach1", "")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound();
     }
 }
